b37fd6f4d8b9dceff1f559284966876ff8fe6bed
Braedon Vrooman <bvrooman@cinci.rr.com>
2017-06-28 20:03:29 +0000

    Dawg It Up

--- Dawgshop/script.js
+++ Dawgshop/script.js
@@ -1,5 +1,7 @@
+
 document.getElementById('go').onclick = function() {
 var inputText = $('#text-input').val();
 var thugifiedText = inputText + ", dawg";
 document.getElementById('output').innerHTML = thugifiedText;
 };
+
